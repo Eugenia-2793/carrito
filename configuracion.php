e78c78739d8243b75f7c84be2db974e105c9a90e
Eugenia Garcia Ruiz <you@example.com>
2021-11-03 12:44:45 -0300

    rutas en configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -5,8 +5,6 @@ $PROYECTO = "carrito";
 $GLOBALS['ROOT'] = $_SERVER['DOCUMENT_ROOT'] . "/carrito/";
 
 $INICIO = "Location:http://" . $_SERVER['HTTP_HOST'] . "$PROYECTO" . "/vista/index/home.php";
-//C:\xampp\htdocs\CarritoCompras\CarritoCompras\configuracion.php
 
 include_once("utiles/funciones.php");
 
-//echo $_SERVER['DOCUMENT_ROOT'];
